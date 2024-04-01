cb3a9e979e07d5ae5837dc291fe4654e958bde0a
Rimma Maksiutova <nnearobot@gmail.com>
2024-04-01 23:01:35 +0900

    ItemList class: add items and calculate total price

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,5 +4,56 @@ require_relative "cash_register/version"
 
 module CashRegister
   class Error < StandardError; end
-  # Your code goes here...
+
+
+  class ItemList
+    def initialize
+      @items = []
+      @net_price = 0
+      @discount = 0
+    end
+
+    def add_item(item)
+      @items << item
+
+      # reset the net price and discount as the items have changed
+      @net_price = 0
+      @discount = 0
+    end
+
+    def total_price
+      @items.sum { |item| item[:price] }
+    end
+
+    def apply_promotions
+      # TODO: apply the promotion rules
+      net_price = total_price
+
+      return net_price
+    end
+
+    def net_price
+      if @net_price == 0
+        @net_price = apply_promotions
+        @discount = total_price - @net_price
+      end
+
+      @net_price
+    end
+
+    def discount
+      # we should check here the net_price because discount can be 0 even if net_price is calculated
+      if @net_price == 0
+        @net_price = apply_promotions
+        @discount = total_price - @net_price
+      end
+
+      @discount
+    end
+
+    def to_s
+      "Total price: JPY #{total_price}, Discount: JPY #{discount}, Net price: JPY #{net_price}"
+    end
+  end
+
 end
--- test/test_cash_register.rb
+++ test/test_cash_register.rb
@@ -7,7 +7,44 @@ class TestCashRegister < Minitest::Test
     refute_nil ::CashRegister::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+
+  def test_empty_cart
+    cash_register = CashRegister::ItemList.new
+    assert_equal 0,
+      cash_register.net_price
   end
+
+
+  def test_add_item
+    cash_register = CashRegister::ItemList.new
+    cash_register.add_item({ SKU: "A", price: 1137 })
+
+    assert_equal 1137, cash_register.total_price
+    assert_equal 0, cash_register.discount
+    assert_equal 1137, cash_register.net_price
+  end
+
+
+  # If the promotion rules change, we can should the test case to reflect the new rules
+
+  def test_add_different_items
+    items = [
+      { SKU: "A", price: 3000 },
+      { SKU: "B", price: 2000 },
+      { SKU: "C", price: 5000 },
+      { SKU: "D", price: 1500 }
+    ]
+
+    cash_register = CashRegister::ItemList.new
+    items.each do |item|
+      cash_register.add_item(item)
+    end
+
+    assert_equal 11500, cash_register.total_price
+    assert_equal 0, cash_register.discount
+    assert_equal 11500, cash_register.net_price
+  end
+
+
+
 end
